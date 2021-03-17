0a12f815f3cc82735e42de39313de20d6e5de9c9
j-dogcoder <j.dogcoder@gmail.com>
2021-03-17 13:21:38 -0400

    More removal of commented out filenames

--- Banking/CardDetail/CardInfoView.swift
+++ Banking/CardDetail/CardInfoView.swift
@@ -1,10 +1,3 @@
-//
-//  CardInfoView.swift
-//  Banking
-//
-//  Created by Anik on 1/8/20.
-//
-
 import SwiftUI
 
 struct CardInfoView: View {
--- Banking/CardDetail/ExpenceView.swift
+++ Banking/CardDetail/ExpenceView.swift
@@ -1,10 +1,3 @@
-//
-//  ExpenceView.swift
-//  Banking
-//
-//  Created by Anik on 1/8/20.
-//
-
 import SwiftUI
 
 struct ExpenceView: View {
